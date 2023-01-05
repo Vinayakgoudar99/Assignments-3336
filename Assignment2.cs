2954a5ad7cc34fa74f4539f92359b879c0a73784
Vinayakgoudar99 <120566051+Vinayakgoudar99@users.noreply.github.com>
2023-01-05 17:18:32 +0530

    Add files via upload

---
+++ Assignment2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class Assignment2
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("even numbers are" );
+
+                for( int i=0;i<numbers.Length;i++)
+                 {
+                    if(numbers[i]%2==0)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+                 }
+
+            Console.WriteLine("Odd numbers are");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+        }
+    }
+}
