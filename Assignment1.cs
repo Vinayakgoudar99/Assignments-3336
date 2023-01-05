bda8901264f021e577560743ca3d25a06349df3a
Vinayakgoudar99 <120566051+Vinayakgoudar99@users.noreply.github.com>
2023-01-05 17:16:36 +0530

    Add files via upload

---
+++ Assignment1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class Assignment1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Manimun rang of Byte is "+byte.MinValue+" and Maximum is "+byte.MaxValue);
+
+            Console.WriteLine("Manimun rang of short is " + short.MinValue + " and Maximum is " + short.MaxValue);
+
+            Console.WriteLine("Manimun rang of int is " + int.MinValue + " and Maximum is " + int.MaxValue);
+
+            Console.WriteLine("Manimun rang of decimal is " + decimal.MinValue + " and Maximum is " + decimal.MaxValue);
+
+            Console.WriteLine("Manimun rang of long is " + long.MinValue + " and Maximum is " + long.MaxValue);
+
+
+            Console.WriteLine("Manimun rang of float is " + float.MinValue + " and Maximum is " + float.MaxValue);
+
+            Console.WriteLine("Manimun rang of double is " + double.MinValue + " and Maximum is " + double.MaxValue);
+
+        }
+    }
+}
