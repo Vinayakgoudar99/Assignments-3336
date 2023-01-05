ae2eb174aef485de1ac7c6d08e2e64ef8b631469
Vinayakgoudar99 <120566051+Vinayakgoudar99@users.noreply.github.com>
2023-01-05 17:20:59 +0530

    Add files via upload

---
+++ ReverseString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Assignment_9
+    {
+        static string reverseString(string input)
+        {
+            string[] arr = input.Split(' ');
+            string output = "";
+
+            for(int i=arr.Length-1;i>=0;i--)
+            {
+                output += arr[i];
+                output += " ";
+
+            }
+
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the input to reverse");
+            string input = Console.ReadLine();
+            string output=reverseString(input);
+
+            Console.WriteLine(output);
+        }
+    }
+}
